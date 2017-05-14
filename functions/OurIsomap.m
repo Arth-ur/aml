0f110a4e9f8ff6541f926cc2a97c8ab032db7e60
Arthur <Arth-ur@users.noreply.github.com>
2017-05-14 23:42:30 +0200

    Minor fix in OurIsomap

--- functions/OurIsomap.m
+++ functions/OurIsomap.m
@@ -1,27 +1,25 @@
 function OurIsomap( X , options)
 %% Compute Isomap with ML_toolbox
 
-if(length(X)>=1000)
-    warning('number of data is bigger than 1000, time expected in dijkstra quite long')
+if(length(X)>1000)
+    warning(['number of data points is larger than 1000, expect time '...
+        'spent in djikstra to be quite long'])
 end
 [proj_ISO_X, mappingISO]  = ml_projection(X,options);
 
-% try
-%     [proj_ISO_X, mappingISO]  = ml_projection(X,options);
-% catch
-%     error('Please enter a higher number of neighbors')
-% end
-
 if length(proj_ISO_X)<length(X)
-    warning('Graph has disconnected components! \n Only %d points from the largest connected component were projected! \n There Ids are stored in: mappingISO.conn_comp.', length(proj_ISO_X))
+    warning(['Graph has disconnected components!\n'...
+        'Only %d points from the largest connected component were'...
+        'projected!\n Their Ids are stored in: mappingISO.conn_comp.',...
+        length(proj_ISO_X)])
 end
 
 % Plot EigenValues to try to find the optimal "p"
 if exist('h3a','var') && isvalid(h3a), delete(h3a);end
 h3a = ml_plot_eigenvalues(diag(mappingISO.val));
 
-options.title        = [options.name, ' : Projected data with Isomap'];
-ml_plot_data(proj_ISO_X(:,[2:3]), options);
+options.title = [options.name ': Projected data with Isomap'];
+ml_plot_data(proj_ISO_X(:,2:3), options);
 legend off
 ax=gca;
 ax.XLabel.FontSize=16;
