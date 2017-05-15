563631f88f4a1213ff0295a50ab4af4c5aaaca65
JulienDiTria <ditria.julien@hotmail.com>
2017-05-15 17:26:03 +0200

    Add complexity analysis for eigenmap

--- demo_script.m
+++ demo_script.m
@@ -24,14 +24,15 @@ disp('setup done')
 
 %% Eigenmap
 clc
+close all
 
 disp ('Running Laplacian Eigenmap')
 
 options = [];
 options.method_name       = 'Laplacian';
-options.nbDimensions      = 10;  % Number of Eigenvectors to compute.
-options.neighbors         = 30;  % Number of k-NN for Adjacency Graph
-options.sigma             = 0.5;   % Sigma for Similarity Matrix
+options.nbDimensions      = 3;  % Number of Eigenvectors to compute.
+options.neighbors         = 10;  % Number of k-NN for Adjacency Graph
+options.sigma             = 10;   % Sigma for Similarity Matrix
 options.labels            = data.labels;
 options.title             = [data.name, ' : Original data'];
 ml_plot_data(data.dataset, options);
@@ -42,8 +43,63 @@ ax.XLabel.FontSize=16;
 ax.YLabel.FontSize=16;
 OurEigenmap( data, options)
 
+%% Complexity analysis
+yn = 'n';
+if exist('functions/output/timing-eigenmap.mat', 'file')
+    yn = input(['A file "timing-eigenmap.dat" already esists in the '...
+    'output directory. Do you want to reuse previously generated '...
+    'data (Y/n)? '], 's');
+end
+if yn == 'n'
+    ns = 5;
+    fprintf('Generating data...\n');
+    Ns = linspace(500,5000,ns);
+    ntries = 10;
+    times = zeros(ns,ntries);
+    for i=1:ns
+        for j=1:ntries
+            [X, labels] = swissroll2(Ns(i), 'ShowFigures', false);
+            tic
+            [proj_LAP_X, mappingLAP]  = ml_projection(X,options);
+            times(i,j) = toc;
+        end
+    end
+    save('functions/output/timing-eigenmap', 'times', 'Ns')
+    fprintf('Done! Data saved to file output/timing-eigenmap.dat\n');
+else
+    fprintf('Loading previously generated data from file output/timing-eigenmap.dat...\n');
+    load('functions/output/timing-eigenmap')
+    fprintf('Done! Data loaded from file output/timing-eigenmap.dat\n');
+end
+
+boxplot(times', Ns)
+xlabel('N','Interpreter','latex')
+ylabel('time (s)','Interpreter','latex')
+title('Eigenmap timing','Interpreter','latex')
+hold on
+
+n=Ns;
+t=2*n.*log(n)*10^-5-0.05;
+
+plot(1:5,t);
+
+axis([-Inf Inf -Inf Inf])
+title('Eigenmap timing','Interpreter', 'Latex','fontsize',20)
+xlabel('N','Interpreter','latex')
+ylabel('Time [s]','Interpreter','latex')
+grid on
+set(gca,'TickLabelInterpreter', 'latex', 'fontsize',16)
+
+h = figure(1);
+set(h,'Units','Inches');
+pos = get(h,'Position');
+set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print('functions/output/timing-eigenmap', '-dpdf','-r0')
+fprintf('Figure saved to file timing-eigenmap.pdf!\n')
+
+
 %% Isomap
-clc
+% clc
 close all
 disp ('Running Isomap')
 
@@ -91,7 +147,8 @@ zlabel('z', 'Interpreter', 'Latex')
 save2pdf(figure(1),'isomap-adjacency-graph-k-5')
 
 %% Complexity analysis
-yn = 'Y';
+yn = 'n';
+ns=7;
 if exist('output/timing-isomap.mat', 'file')
     yn = input(['A file "timing-isomap.dat" already esists in the '...
     'output directory. Do you want to reuse previously generated '...
@@ -106,7 +163,7 @@ if yn == 'n'
         for j=1:ntries
             [X, ~] = swissroll2(Ns(i), 'ShowFigures', false);
             tic
-            Isomap(X');
+            [proj_ISO_X, mappingISO]  = ml_projection(X,options);
             times(i,j) = toc;
         end
     end
@@ -117,9 +174,11 @@ else
     load('output/timing-isomap')
     fprintf('Done! Data loaded from file output/timing-isomap.dat\n');
 end
+
+
 boxplot(times', Ns)
 xlabel('N','Interpreter','latex')
-ylabel('time (s)','Interpreter','latex')
+ylabel('Time [s]','Interpreter','latex')
 title('Isomap timing','Interpreter','latex')
 h = figure(1);
 set(h,'Units','Inches');
--- functions/OurEigenmap.m
+++ functions/OurEigenmap.m
@@ -21,7 +21,8 @@ plot_options              = [];
 plot_options.is_eig       = false;
 plot_options.labels       = labels;
 plot_options.plot_labels  = {'$y_1$','$y_2$','$y_3$'};
-plot_options.title        = [data.name, ' : Projected data with Laplacian Eigenmaps'];
+% plot_options.title        = [data.name, ' : Projected data with Laplacian Eigenmaps'];
+plot_options.title        = ['Eigenmap Projection $k = 10$'];
 if exist('h4','var') && isvalid(h4), delete(h4);end
 h4 = ml_plot_data(proj_LAP_X(:,[1:3]),plot_options);
 legend off
@@ -29,6 +30,21 @@ ax=gca;
 ax.XLabel.FontSize=16;
 ax.YLabel.FontSize=16;
 ax.ZLabel.FontSize=16;
+
+h=figure;
+scatter(proj_LAP_X(:,1),proj_LAP_X(:,3),[],options.labels,'filled');
+title('Eigenmap Projection $k = 10$', 'Interpreter', 'Latex','fontsize',20)
+xlabel('Y1','Interpreter','latex')
+ylabel('Y3','Interpreter','latex')
+grid on
+set(gca,'TickLabelInterpreter', 'latex', 'fontsize',16)
+
+set(h,'Units','Inches');
+pos = get(h,'Position');
+set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print('functions/output/FunSwissroll2_Laplacian_dim_3_knn_10_sig_10', '-dpdf','-r0')
+fprintf('Figure saved to file isomap-swissroll.pdf!\n')
+
 disp('end of Eigenmap function')
 end
 
