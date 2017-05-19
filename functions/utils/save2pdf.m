04093e80767d5f506b559cdfa247597b16182169
Arthur Gay <Arth-ur@users.noreply.github.com>
2017-05-19 17:27:39 +0200

    save2pdf

--- functions/utils/save2pdf.m
+++ functions/utils/save2pdf.m
@@ -1,7 +1,12 @@
 function save2pdf( h, file)
 %SAVE2PDF Save figure h to pdf file
 
-parentdir = 'functions/output';
+parentdir = 'output';
+
+if ~exist(parentdir, 'dir')
+   mkdir(parentdir); 
+end
+
 path = sprintf('%s/%s', parentdir, file);
 
 set(h,'Units','Inches');
